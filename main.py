b1a218b0e733456963869ab9dc624fa2faa2a94c
IrynaBarnych <iryna.barnych@gmail.com>
2023-11-04 22:07:08 +0200

    шаблон

---
+++ main.py
@@ -0,0 +1,40 @@
+# Завдання
+#
+
+
+class City:
+    def __init__(self, city_code):
+        self.
+        self. =
+        self. =
+        self. =
+        self. =
+        self.city_code = city_code
+
+    def get_(self):
+        return self.
+
+    def get_(self):
+        return self.
+
+    def get_(self):
+        return self.
+    def get_(self):
+        return self.
+
+    def get_e(self):
+        return self.
+
+    def get_city_code(self):
+        return self.city_code
+
+# Створення екземпляра класу
+ = ()
+
+# Виведення інформації про Місто
+print(f"Назва міста: {()}")
+print(f"Назва регіону: {()}")
+print(f"Назва країни: {()}")
+print(f"Кількість жителів у місті: {()}")
+print(f"Поштовий індекс: {()}")
+print(f"Код міста: {City1.get_city_code()}")
