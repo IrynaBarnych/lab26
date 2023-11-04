2527f3188f3a6265542e422878d40bed63039a62
IrynaBarnych <iryna.barnych@gmail.com>
2023-11-04 22:28:40 +0200

    додавання дробів

--- main.py
+++ main.py
@@ -11,5 +11,16 @@ class Fraction:
         self.numerator = numerator
         self.denominator = denominator
 
+    def __str__(self):
+        return f"{self.numerator}/{self.denominator}"
+
+    def __add__(self, other):
+        new_numerator = self.numerator * other.denominator + other.numerator * self.denominator
+        new_denominator = self.denominator * other.denominator #спільний знаменник
+        return Fraction(new_numerator, new_denominator)
+
+
 fraction1 = Fraction(4, 7)
 fraction2 = Fraction(5, 8)
+
+print(fraction1 + fraction2)
