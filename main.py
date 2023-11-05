964d2219e6649e12f06468270fb9f99d420b5289
IrynaBarnych <iryna.barnych@gmail.com>
2023-11-05 21:05:11 +0200

    код працює виправила помилки

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 # Завдання 4
 # До вже реалізованого класу «Книга» додайте необхідні перевантажені методи та оператори.
 
-
 class Book:
     def __init__(self, title, author, genre, date):
         self.title = title
         self.author = author
         self.genre = genre
-        self.date = int(date)
+        self.date = date
 
     def __sub__(self, other):
         return self.date - other.date
@@ -18,10 +17,10 @@ class Book:
     def Book(self):
         print(f"Назва книги: {self.title}, Автор: {self.author}, Жанр: {self.genre}, Дата випуску: {self.date}")
 
-my_book_info1 = Book("Кобзар", "Т. Г. Шевченко", "різні літературні жанри", "1840")
+my_book_info1 = Book("Кобзар", "Т. Г. Шевченко", "різні літературні жанри", 1840)
 my_book_info1.Book()
 
-my_book_info2 = Book("Енеїда", "І. П. Котляревський", "комічна епічна поема", "1798")
+my_book_info2 = Book("Енеїда", "І. П. Котляревський", "комічна епічна поема", 1798)
 my_book_info2.Book()
 
 date_difference = my_book_info1 - my_book_info2
