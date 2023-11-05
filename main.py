896108fc8e5e10d6fa33770aa2898ea3300be29c
IrynaBarnych <iryna.barnych@gmail.com>
2023-11-05 14:27:44 +0200

    розрахувала відстань від центру міста до стадіонів по теоремі Піфагора і прорахувала різницю

--- main.py
+++ main.py
@@ -4,7 +4,7 @@
 # До вже реалізованого класу «Стадіон» додайте необхідні перевантажені методи та оператори.
 
 class Stadium:
-    def __init__(self, name, opening_date, country, city, capacity, length, width):
+    def __init__(self, name, opening_date, country, city, capacity, length, width, distance_a, distance_b):
         self.name = name
         self.opening_date = opening_date
         self.country = country
@@ -12,6 +12,8 @@ class Stadium:
         self.capacity = capacity
         self.length = float(length)  # Перетворення в числове значення
         self.width = float(width)
+        self.distance_a = float(distance_a)
+        self.distance_b = float(distance_b)
     def __sub__(self, other):
         return (self.capacity - other.capacity)
 
@@ -20,12 +22,17 @@ class Stadium:
         square2 = other.length * other.width
         return abs(square1 - square2)
 
+    def __add__(self, other):
+        distance1 = self.distance_a ** 2 + self.distance_b ** 2
+        distance2 = other.distance_a ** 2 + other.distance_b ** 2
+        return abs(distance1 - distance2)
+
 
 
 stadion1 = Stadium("Олімпійський", "22.09.1923", "Україна", "Київ", 70050,
-                   "150", "100")
+                   "150", "100", "1", "2")
 stadion2 = Stadium("Донбас-Арена", "29.09.2009", "Україна", "Донецьк",  525180,
-                   "105", "68")
+                   "105", "68", "2", "3")
 
 
 difference = stadion1 - stadion2
@@ -33,3 +40,6 @@ print(f"Різниця в місткості стадіонів: {difference} о
 
 area_difference = stadion1 * stadion2
 print(f"Різниця в площі стадіонів: {area_difference} квадратних метрів")
+
+plane_distance = stadion1 + stadion2
+print(f"Різниця від центру міста до стадіонів: {plane_distance}")
