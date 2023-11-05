fd9627245f141c4226c9e99a0ba7991715cdb143
IrynaBarnych <iryna.barnych@gmail.com>
2023-11-05 15:10:22 +0200

    додала і виправила код з минулої роботи

--- main.py
+++ main.py
@@ -2,4 +2,19 @@
 # До вже реалізованого класу «Книга» додайте необхідні перевантажені методи та оператори.
 
 
+class Book:
+    def __init__(self, title, author, genre):
+        self.title = title
+        self.author = author
+        self.genre = genre
+
+    def print_info(self):
+        print(f"Назва книги: {self.title}, Автор: {self.author}, Жанр: {self.genre}")
+
+# Створення екземпляра класу
+my_book_info = Book("Кобзар", "Т. Г. Шевченко", "вірші")
+my_book_info.print_info()
+
+
+
 
