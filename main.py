993a42e8288a80e2866a65eea26dc2cabd38e189
IrynaBarnych <iryna.barnych@gmail.com>
2023-11-05 15:02:02 +0200

    додала код прорахунку різниці в роках і вартості (перерахувала вартість з долару у грибні і вирахувала різницю)

--- main.py
+++ main.py
@@ -2,18 +2,39 @@
 # До вже реалізованого класу «Автомобіль» додайте необхідні перевантажені методи та оператори.
 
 class Car:
-    def __init__(self, brand, model, year):
+    def __init__(self, brand, model, year, cost_USA, dolar):
         self.brand = brand
         self.model = model
         self.year = year
+        self.cost_USA = float(cost_USA)  # Перетворення в числове значення
+        self.dolar = float(dolar)  # Перетворення в числове значення
 
     def start_engine(self):
         print("Двигун запущено")
 
-    def car_info(self):
-        print(f"Марка автомобіля: {self.brand}, Модель: {self.model}, Рік випуску: {self.year}")
+    def __sub__(self, other):
+        return self.year - other.year
 
-# Створення екземпляра класу
-car_instance = Car("BMW", "X5", 2022)
-car_instance.start_engine()
-car_instance.car_info()
+    def __mul__(self, other):
+        cost1 = self.cost_USA * self.dolar
+        cost2 = other.cost_USA * other.dolar
+        return abs(cost1 - cost2)
+
+    def car_info(self):
+        print(f"Марка автомобіля: {self.brand}, Модель: {self.model}, Рік випуску: {self.year},"
+              f" Вартість у Доларах: {self.cost_USA}, Курс Долару: {self.dolar}")
+
+# Створення екземплярів класу
+car_instance1 = Car("BMW", "X5", 2022, 13999, 36)
+car_instance1.start_engine()
+car_instance1.car_info()
+car_instance2 = Car("BMW", "X6", 2023, 17599, 36)
+car_instance2.start_engine()
+car_instance2.car_info()
+
+difference = car_instance1 - car_instance2
+print(f"Різниця в роках: {difference}")
+
+cost_difference = car_instance1 * car_instance2
+print(f"Різниця в вартості: {cost_difference} Гривень")
+
